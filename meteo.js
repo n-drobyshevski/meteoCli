799a55a005ccc9ba59b68ca060fb1beaf93b985e
Nick <n.drobyshevski@pm.me>
2022-07-25 23:04:26 +0200

    Add: Save keyValue to .json data file

--- meteo.js
+++ meteo.js
@@ -1,19 +1,27 @@
 #!/usr/bin/env node
+// ab1c76e9b0b9a762d74b32366a201ba9
 
 import { getArgs } from "./helpers/args.js";
 import { printHelp } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
 
+const saveToken = async (token) => {
+    if (token) {
+        await saveKeyValue('token', token)
+    }
+}
 const cliInit = () => {
     let args = getArgs(process.argv);
-    console.log(args);
     if (args.h) {
         return printHelp();
     }
     if (args.c) {
+        const city = args.c;
+        console.log(city);
         return saveCity();
     }
     if (args.t) {
-        return saveToken();
+        return saveToken(args.t);
     }
 
 }
---
+++ services/storage.service.js
@@ -0,0 +1,30 @@
+import { promises } from "fs"
+import { join } from "path"
+import { homedir } from "os"
+
+const FILE_PATH = join(homedir(), 'weather_data.json')
+
+const isExist = async (file_path) => {
+    try{
+        await promises.stat(file_path)
+        return true 
+    } catch{
+        return false
+    }
+}
+
+const saveKeyValue = async (key, value) => {
+    console.log(key)
+    console.log(value)
+    let data = {};
+    if (await isExist(FILE_PATH)){  
+        const file = await promises.readFile(FILE_PATH)
+        data = JSON.parse(file)
+    }
+    data[key] = value;
+    
+    await promises.writeFile(FILE_PATH, JSON.stringify(data))
+
+}
+
+export { saveKeyValue }
